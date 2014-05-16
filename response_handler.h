142a715ac06ff85982e1065c26ddd86eafc61f62
James Farrell <jfarrell468@gmail.com>
2014-05-16 14:31:21 -0700

    Some comments, etc.

--- response_handler.h
+++ response_handler.h
@@ -2,6 +2,11 @@ class NginxConfig;
 
 class ResponseHandler {
  public:
+  // Initialize the response handler. The config object is just the portion
+  // that applies to the response handler.
   virtual void Initialize(const NginxConfig& config) = 0;
-  virtual Response HandleRequest(const Request& request) = 0;
+
+  // Generates response for the request.
+  // Returns true if successful. Returns the response in Response object.
+  virtual bool HandleRequest(const Request& request, Response* response) = 0;
 }
