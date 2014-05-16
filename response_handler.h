5ebda4cb6a106eafeeda916e76052df44c292af7
James Farrell <jfarrell468@gmail.com>
2014-05-16 16:47:42 -0700

    Fix up the handler interface and add some comments.

--- response_handler.h
+++ response_handler.h
@@ -1,12 +1,29 @@
+#include <string>
+
 class NginxConfig;
 
-class ResponseHandler {
+// Interface for response handlers.
+// * All of your handlers (echo, static file, etc.) should inherit from this
+//   class.
+// * Response handler objects should be long-lived -- they should be created
+//   once, at server startup.
+class ResponseHandlerInterface {
  public:
-  // Initialize the response handler. The config object is just the portion
-  // that applies to the response handler.
-  virtual void Initialize(const NginxConfig& config) = 0;
+  // Initializes the response handler object. The config argument is just the
+  // portion that applies to the response handler.
+  // Returns true if successful.
+  // 
+  // For example, when constructing the config for the following stanza:
+  // handler StaticFileHandler {
+  //   location /static/;
+  //   document_root /foo/bar;
+  // }
+  // The call to initialize would only contain the config data for the location
+  // and the document_root.
+  virtual bool Initialize(const NginxConfig& config) = 0;
 
-  // Generates response for the request.
-  // Returns true if successful. Returns the response in Response object.
-  virtual bool HandleRequest(const Request& request, Response* response) = 0;
-}
+  // Parses a HTTP request and generates a HTTP response to the request.
+  // Returns true if successful.
+  virtual bool HandleRequest(const std::string& request,
+  	                         std::string* response) = 0;
+};
